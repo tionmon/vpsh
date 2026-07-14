2f66eb92a8c2b9e9f61385491a3bfe2a32eaa071
Tionmon <tionmon17@gmail.com>
2026-07-14 15:34:57 +0800

    Add one-click Caddy installer

---
+++ run/incaddy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+sudo apt update
+sudo apt install -y \
+  debian-keyring \
+  debian-archive-keyring \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg
+
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' \
+  | sudo gpg --dearmor --yes -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' \
+  | sudo tee /etc/apt/sources.list.d/caddy-stable.list >/dev/null
+
+sudo apt update
+sudo apt install -y caddy
